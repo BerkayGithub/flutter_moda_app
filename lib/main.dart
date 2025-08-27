07c547c56cb76067491ac4fc2a55b78d08acb902
BerkayGithub <berkay.danis@hotmail.com>
2025-08-27 23:02:41 +0300

    Ana sayfa bitti

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,44 @@ class AnaSayfa extends StatefulWidget {
   State<AnaSayfa> createState() => _AnaSayfaState();
 }
 
-class _AnaSayfaState extends State<AnaSayfa> {
+class _AnaSayfaState extends State<AnaSayfa>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+          controller: tabController,
+          tabs: [
+            Tab(icon: Icon(Icons.more, color: Colors.grey, size: 16)),
+            Tab(icon: Icon(Icons.play_arrow, color: Colors.grey, size: 16)),
+            Tab(icon: Icon(Icons.navigation, color: Colors.grey, size: 16)),
+            Tab(
+              icon: Icon(
+                Icons.supervised_user_circle,
+                color: Colors.grey,
+                size: 16,
+              ),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -45,12 +79,17 @@ class _AnaSayfaState extends State<AnaSayfa> {
           ),
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.camera_alt), color: Colors.grey),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.camera_alt),
+            color: Colors.grey,
+          ),
         ],
       ),
       body: ListView(
         padding: EdgeInsets.only(top: 10),
         children: <Widget>[
+          //Üst taraf profil listesi
           Container(
             color: Colors.transparent,
             height: 150,
@@ -73,7 +112,212 @@ class _AnaSayfaState extends State<AnaSayfa> {
                 SizedBox(width: 30),
               ],
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(15),
+              elevation: 4,
+              child: Container(
+                height: 550,
+                width: double.infinity,
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/model1.jpeg'),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(40),
+                          ),
+                          height: 75,
+                          width: 75,
+                        ),
+                        SizedBox(width: 20),
+                        Container(
+                          width: MediaQuery.of(context).size.width - 170,
+                          alignment: Alignment.centerLeft,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Daisy",
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "34 minutes ago",
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 14,
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Icon(
+                          Icons.more_vert,
+                          color: Colors.grey.shade600,
+                          size: 22,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      "This official website features a ribbed knit zipper jacket that is modern and stylish. It looks very temparament and is recommend to friends",
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 200,
+                          width: (MediaQuery.of(context).size.width - 50) / 2,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("assets/modelgrid1.jpeg"),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Column(
+                          children: <Widget>[
+                            Container(
+                              height: 95,
+                              width:
+                                  (MediaQuery.of(context).size.width - 100) / 2,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage("assets/modelgrid2.jpeg"),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Container(
+                              height: 95,
+                              width:
+                                  (MediaQuery.of(context).size.width - 100) / 2,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage("assets/modelgrid3.jpeg"),
+                                  fit: BoxFit.cover,
+                                ),
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 25,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.brown.withAlpha(60),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "# Louis Vitton",
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 10,
+                              color: Colors.brown,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          height: 25,
+                          width: 75,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.brown.withAlpha(60),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Text(
+                            "# Chloe",
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontSize: 10,
+                              color: Colors.brown,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Divider(),
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.reply,
+                          color: Colors.brown.withAlpha(60),
+                          size: 30,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "1.7 k",
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(width: 25),
+                        Icon(
+                          Icons.comment,
+                          color: Colors.brown.withAlpha(60),
+                          size: 30,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "325",
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 16,
+                          ),
+                        ),
+
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 2 - 30,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Icon(Icons.favorite, color: Colors.red, size: 30),
+                              SizedBox(width: 5),
+                              Text(
+                                "2.3 k",
+                                style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -89,7 +333,10 @@ class _AnaSayfaState extends State<AnaSayfa> {
               width: 75,
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(40),
-                image: DecorationImage(image: AssetImage(imagePath), fit: BoxFit.cover),
+                image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             Positioned(
@@ -100,10 +347,13 @@ class _AnaSayfaState extends State<AnaSayfa> {
                 width: 25,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(30),
-                  image: DecorationImage(image: AssetImage(logoPath), fit: BoxFit.cover)
+                  image: DecorationImage(
+                    image: AssetImage(logoPath),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            )
+            ),
           ],
         ),
         SizedBox(height: 10),
@@ -112,13 +362,15 @@ class _AnaSayfaState extends State<AnaSayfa> {
           width: 75,
           decoration: BoxDecoration(
             color: Colors.brown,
-            borderRadius: BorderRadius.circular(15)
+            borderRadius: BorderRadius.circular(15),
           ),
           alignment: Alignment.center,
-          child: Text("Follow", style: TextStyle(fontFamily: 'Montserrat', color: Colors.white)),
-        )
+          child: Text(
+            "Follow",
+            style: TextStyle(fontFamily: 'Montserrat', color: Colors.white),
+          ),
+        ),
       ],
     );
   }
-
 }
